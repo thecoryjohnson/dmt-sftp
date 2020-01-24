c0588cfff870ba4b08b29d6c955d91f56d0b7edf
Cory Johnson <cory.johnson1@xpo.com>
2020-01-24 18:33:53 -0500

    chore: configuration
    setup logging and appsettings

--- src/DMT.SFTP.Service/Program.cs
+++ src/DMT.SFTP.Service/Program.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+using Serilog;
+using Serilog.Events;
 
 namespace DMT.SFTP.Service
 {
@@ -11,14 +14,30 @@ namespace DMT.SFTP.Service
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            try
+            {
+                CreateHostBuilder(args).Build().Run();
+                return;
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Error starting service");
+                return;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureServices((hostContext, services) =>
                 {
+                    services.Configure<AppSettings>(hostContext.Configuration.GetSection("AppSettings"));
                     services.AddHostedService<Worker>();
-                });
+                    services.AddTransient(_ => _.GetRequiredService<IOptions<AppSettings>>().Value);
+                })
+            .UseSerilog((ctx, config) => { config.ReadFrom.Configuration(ctx.Configuration); });
     }
 }
--- src/DMT.SFTP.Service/Worker.cs
+++ src/DMT.SFTP.Service/Worker.cs
@@ -5,24 +5,76 @@ using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Renci.SshNet;
+using SshNet;
 
 namespace DMT.SFTP.Service
 {
     public class Worker : BackgroundService
     {
         private readonly ILogger<Worker> _logger;
+        private readonly AppSettings _appSettings;
+        private readonly string _userName;
+        private readonly string _password;
+        private readonly string _host;
 
-        public Worker(ILogger<Worker> logger)
+        public Worker(ILogger<Worker> logger, IOptions<AppSettings> appSettings)
         {
             _logger = logger;
+            _appSettings = appSettings.Value;
+            _userName = _appSettings.Username;
+            _password = _appSettings.Password;
+            _host = _appSettings.Host;
+        }
+
+        public override Task StartAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Application is starting");
+            return base.StartAsync(cancellationToken);
+        }
+
+        public override Task StopAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Application is shutting down");
+            return base.StopAsync(cancellationToken);
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             while (!stoppingToken.IsCancellationRequested)
             {
-                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-                await Task.Delay(1000, stoppingToken);
+
+                
+
+                await Task.Delay(TimeSpan.FromMinutes(_appSettings.PollingIntervalMinutes), stoppingToken);
+            }
+        }
+
+        private List<string> GetNewInboundFileNamesFromFTP()
+        {
+            var inboundFiles = new List<string>();
+
+            using (var sftpClient = new Renci.SshNet.SftpClient(_host, _userName, _password))
+            {
+                sftpClient.Connect();
+            }
+
+                return inboundFiles;
+        }
+
+        private void ListSftpDirectory(SftpClient sftpClient, string directory, ref List<string> files)
+        {
+            foreach (var sftpEntry in sftpClient.ListDirectory(directory))
+            {
+                if(sftpEntry.IsRegularFile)
+                {
+
+                }
+                else
+                {
+                    _logger.LogWarning($"Not a regular file: {sftpEntry.FullName}");
+                }
             }
         }
     }
