67df110765c307062ba23158541a0cd40a2cbda4
Cory Johnson <cory.johnson1@xpo.com>
2020-01-25 10:41:57 -0500

    feature: complete upload logic
    finish out business logic to upload from GK to TI

--- src/DMT.SFTP.Service/Worker.cs
+++ src/DMT.SFTP.Service/Worker.cs
@@ -47,21 +47,25 @@ namespace DMT.SFTP.Service
             {
                 using (var sftpClient = new SftpClient(_host, 22, _userName, _password))
                 {
-                    sftpClient.Connect();
-
-                    var inboundContactFiles = GetNewInboundFileNamesFromTi(sftpClient);
-                    var outboundContactFiles = GetNewOutboundFileNamesFromGk(_appSettings.GkContactOutboundDirectory);
-
                     try
                     {
-                        
+                        sftpClient.Connect();
+
+                        var inboundContactFiles = GetNewInboundFileNamesFromTi(sftpClient);
+                        var outboundContactFiles = GetNewOutboundFileNamesFromGk(_appSettings.GkContactOutboundDirectory);
+
+                        // Files inbound to Golden Key, from True Interation
                         if (inboundContactFiles.Count > 0)
                         {
                             _logger.LogInformation($"{inboundContactFiles.Count} files found in the true interation outbound site");
 
                             foreach (var sourceFile in inboundContactFiles)
                             {
+                                _logger.LogInformation($"Moving WordPress extract file {sourceFile.Name}");
+
                                 MoveFileFromTiSftpSiteToGk(sftpClient, sourceFile);
+
+                                _logger.LogInformation($"{sourceFile.Name} moved from remote site to Gk");
                             }
                         }
                         else
@@ -69,12 +73,17 @@ namespace DMT.SFTP.Service
                             _logger.LogInformation("No new contact files found in the true interaction outbound site");
                         }
 
+                        // Files inbound to True Interation, from Golden Key
                         if (outboundContactFiles.Count > 0)
                         {
                             _logger.LogInformation($"{outboundContactFiles.Count} files found in the golden key outbound site");
                             foreach (var sourceFile in outboundContactFiles)
                             {
-                                _logger.LogInformation($"Moving file {sourceFile}");
+                                _logger.LogInformation($"Moving Netforum extract file {sourceFile}");
+
+                                MoveFileFromGkToTiSftpSite(sftpClient, sourceFile);
+
+                                _logger.LogInformation($"{sourceFile} moved from Gk to remote site");
                             }
                         }
                         else
@@ -92,10 +101,15 @@ namespace DMT.SFTP.Service
                     }
                 }
                 
-                await Task.Delay(2000, stoppingToken);
+                await Task.Delay(TimeSpan.FromMinutes(_appSettings.PollingIntervalMinutes), stoppingToken);
             }
         }
 
+        /// <summary>
+        /// Returns a list of regular files (Wordpress extract) in the TI Contact outbound remote site
+        /// </summary>
+        /// <param name="sftpClient"></param>
+        /// <returns></returns>
         private List<SftpFile> GetNewInboundFileNamesFromTi(SftpClient sftpClient)
         {
             var inboundFiles = new List<SftpFile>();
@@ -105,6 +119,11 @@ namespace DMT.SFTP.Service
             return inboundFiles;
         }
 
+        /// <summary>
+        /// Returns a list of files (Netforum extract) in the GK Contact outbound local site
+        /// </summary>
+        /// <param name="sourceDirectory"></param>
+        /// <returns></returns>
         private List<string> GetNewOutboundFileNamesFromGk(string sourceDirectory)
         {
             var outboundFiles = new List<string>();
@@ -115,7 +134,7 @@ namespace DMT.SFTP.Service
                 var files = directoryInfo.GetFiles();
                 foreach(var file in files)
                 {
-                    outboundFiles.Add(file.FullName);
+                    outboundFiles.Add(file.Name);
                 }
             }
             else
@@ -126,6 +145,12 @@ namespace DMT.SFTP.Service
             return outboundFiles;
         }
 
+        /// <summary>
+        /// Returns a list of files in a SFTP remote site
+        /// </summary>
+        /// <param name="sftpClient"></param>
+        /// <param name="directory"></param>
+        /// <param name="files"></param>
         private void ListSftpDirectory(SftpClient sftpClient, string directory, ref List<SftpFile> files)
         {
             foreach (var sftpEntry in sftpClient.ListDirectory(directory))
@@ -137,6 +162,11 @@ namespace DMT.SFTP.Service
             }
         }
 
+        /// <summary>
+        /// Downloads the Outbound Contact files from Wordpress
+        /// </summary>
+        /// <param name="sftpClient"></param>
+        /// <param name="sourceFile"></param>
         private void MoveFileFromTiSftpSiteToGk(SftpClient sftpClient, SftpFile sourceFile)
         {
             var localFilePath = _appSettings.GkContactInboundDirectory + sourceFile.Name;
@@ -144,6 +174,22 @@ namespace DMT.SFTP.Service
             using (Stream localFile = File.Create(localFilePath))
             {
                 sftpClient.DownloadFile(sourceFile.FullName, localFile);
+            }
+        }
+
+        /// <summary>
+        /// Uploads the Outbound Contact files from Netforum
+        /// </summary>
+        /// <param name="sftpClient"></param>
+        /// <param name="sourceFile"></param>
+        private void MoveFileFromGkToTiSftpSite(SftpClient sftpClient, string sourceFile)
+        {
+            var localFilePath = _appSettings.GkContactOutboundDirectory + sourceFile;
+            var remotePath = _appSettings.TiContactInboundDirectory + sourceFile;
+
+            using (Stream localFile = File.OpenRead(localFilePath))
+            {
+                sftpClient.UploadFile(localFile, remotePath);
             }
         }
     }
